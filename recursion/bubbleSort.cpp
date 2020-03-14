61fe34ae422a9629c0b4636c0f88ba8b40002da3
Himanshu <61385037+himanshu-tawra@users.noreply.github.com>
2020-03-15 00:04:34 +0530

    Create bubbleSort.cpp

---
+++ recursion/bubbleSort.cpp
@@ -0,0 +1,49 @@
+#include<iostream>
+using namespace std;
+
+void BubbleSort(int *a,int n , int i)
+{
+
+    // base case
+    if(i == n-1)
+    {
+        return;
+    }
+
+    for(int j = 0; j < n - 1 - i; j++)
+    {
+        if(a[j] > a[j+1])
+        {
+            swap(a[j] , a[j+1]);
+        }
+    }
+
+    BubbleSort(a,n,i+1);
+}
+
+
+
+void Print(int *a,int n)
+{
+	for(int i=0;i<n;i++)
+    {
+		cout<<a[i]<<' ';
+	}
+	cout<<endl;
+}
+
+
+
+
+
+int main()
+{
+
+	int a[]={4,2,5,6,3,1,0};
+	int n = sizeof(a)/sizeof(int);
+	Print(a,n);
+	BubbleSort(a,n,0);
+	Print(a,n);
+	
+	return 0; 
+}
