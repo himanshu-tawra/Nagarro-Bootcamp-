5215578edef852c01525dfe30a5fa1e23618fe09
himanshu-tawra <himanshu.tawra01@gmail.com>
2020-05-27 02:27:18 +0530

    Binary Tree

---
+++ height.cpp
@@ -0,0 +1,71 @@
+#include<iostream>
+using namespace std;
+
+class Node{
+    public:
+        int data;
+        Node* left;
+        Node* right;
+        Node(int d){
+            data = d;
+            left = right = NULL;
+        }
+};
+
+Node* createBT(){
+
+    int data;
+    cin>>data;
+
+    if(data == -1){
+        return NULL;
+    }
+
+    Node* root = new Node(data);
+    root->left = createBT();
+    root->right = createBT();
+
+    return root;
+}
+
+
+int heightBT(Node* root){
+    if(root == NULL){
+        return 0;
+    }
+
+    int leftHeight = heightBT(root->left);
+    int rightHeight = heightBT(root->right);
+
+    int height = max(leftHeight,rightHeight)+1;
+
+    return height;
+
+}
+void preOrder(Node* root){
+    if(root == NULL){
+        return;
+    }
+    cout<<root->data<<" ";
+    preOrder(root->left);
+    preOrder(root->right);
+}
+
+
+
+
+int main(){
+
+    Node* root = createBT();
+    
+    
+    cout<<"Preorder"<<endl;
+    preOrder(root);
+    cout<<endl;
+
+    int treeHeight = heightBT(root);
+
+    cout<<"Height of tree : "<<treeHeight<<endl;
+  
+    return 0;
+}
